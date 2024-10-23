908eb18977e54d87fbf5650486c254e005a2bbc7
Homa Wong <homawong@gmail.com>
2024-10-23 15:01:58 -0700

    chore: @storybook/experimental-addon-test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "./local-preset.js",
+    "@storybook/experimental-addon-test"
   ],
   framework: {
     name: "@storybook/react-vite",
