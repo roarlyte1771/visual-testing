271fa50d0aa17ff95da17d22444e6946632e58b8
Homa Wong <homawong@gmail.com>
2024-10-23 17:54:43 -0700

    feat: add context proxy

---
+++ src/@vitest/browser/context.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from '@storybook/react'
+import { expect } from '@storybook/test'
+import { cdp } from './context'
+
+export default {
+	title: 'vitest/context',
+	render: () => <></>,
+} satisfies Meta
+
+export const AccessCDP = {
+	play() {
+		expect(cdp()).toBeDefined()
+	},
+}
---
+++ src/@vitest/browser/context.ts
@@ -0,0 +1,84 @@
+import {
+  getByAltText,
+  getByLabelText,
+  getByPlaceholderText,
+  getByRole,
+  getByTestId,
+  getByText,
+  getByTitle,
+} from '@storybook/test'
+import type { BrowserCommands, BrowserPage, CDPSession, Platform } from '@vitest/browser/context'
+import type { SerializedConfig } from 'vitest'
+
+let ctx: Awaited<typeof import('@vitest/browser/context')>
+
+if ((globalThis as any).__vitest_browser__) {
+	ctx = await import('@vitest/browser/context')
+}
+
+// TODO: stub the functions within the `CDPSession`
+// when we better understands the use cases
+export function cdp(): CDPSession {
+	return ctx?.cdp() ?? {}
+}
+
+export const commands = new Proxy<BrowserCommands>({} as any, {
+	get(target, prop) {
+		return (target as any)[prop] ?? (ctx?.commands as any)[prop]
+	},
+})
+
+export const page = new Proxy<BrowserPage>(
+	{
+		getByAltText,
+		getByRole,
+		getByTestId,
+		getByText,
+		getByTitle,
+		getByPlaceholder: getByPlaceholderText,
+		getByLabelText,
+	} as any,
+	{
+		get(target, prop) {
+			return ((target as any)[prop] ?? ctx?.page)
+				? (ctx?.page as any)[prop]
+				: () => {
+						console.info(`\`page.${prop.toString()}\` does not exist when running in browser`)
+					}
+		},
+	},
+)
+
+export const server = new Proxy<{
+  /**
+   * Platform the Vitest server is running on.
+   * The same as calling `process.platform` on the server.
+   */
+  platform: Platform
+  /**
+   * Runtime version of the Vitest server.
+   * The same as calling `process.version` on the server.
+   */
+  version: string
+  /**
+   * Name of the browser provider.
+   */
+  provider: string
+  /**
+   * Name of the current browser.
+   */
+  browser: string
+  /**
+   * Available commands for the browser.
+   * @see {@link https://vitest.dev/guide/browser/commands}
+   */
+  commands: BrowserCommands
+  /**
+   * Serialized test config.
+   */
+  config: SerializedConfig
+}>({} as any, {
+	get(target, prop) {
+		return (target as any)[prop] ?? (ctx?.server as any)[prop]
+	},
+})
