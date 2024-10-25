5973332cfafc784a1b66c711947e404a93830b57
Homa Wong <homawong@gmail.com>
2024-10-25 13:45:38 -0700

    chore: note fix for top-level await

--- src/@vitest/browser/context.ts
+++ src/@vitest/browser/context.ts
@@ -1,11 +1,11 @@
 import {
-  getByAltText,
-  getByLabelText,
-  getByPlaceholderText,
-  getByRole,
-  getByTestId,
-  getByText,
-  getByTitle,
+	getByAltText,
+	getByLabelText,
+	getByPlaceholderText,
+	getByRole,
+	getByTestId,
+	getByText,
+	getByTitle,
 } from '@storybook/test'
 import type { BrowserCommands, BrowserPage, CDPSession, Platform } from '@vitest/browser/context'
 import type { SerializedConfig } from 'vitest'
@@ -14,6 +14,7 @@ let ctx: Awaited<typeof import('@vitest/browser/context')>
 
 if ((globalThis as any).__vitest_browser__) {
 	ctx = await import('@vitest/browser/context')
+	// import('@vitest/browser/context').then((m) => (ctx = m))
 }
 
 // TODO: stub the functions within the `CDPSession`
@@ -50,33 +51,33 @@ export const page = new Proxy<BrowserPage>(
 )
 
 export const server = new Proxy<{
-  /**
-   * Platform the Vitest server is running on.
-   * The same as calling `process.platform` on the server.
-   */
-  platform: Platform
-  /**
-   * Runtime version of the Vitest server.
-   * The same as calling `process.version` on the server.
-   */
-  version: string
-  /**
-   * Name of the browser provider.
-   */
-  provider: string
-  /**
-   * Name of the current browser.
-   */
-  browser: string
-  /**
-   * Available commands for the browser.
-   * @see {@link https://vitest.dev/guide/browser/commands}
-   */
-  commands: BrowserCommands
-  /**
-   * Serialized test config.
-   */
-  config: SerializedConfig
+	/**
+	 * Platform the Vitest server is running on.
+	 * The same as calling `process.platform` on the server.
+	 */
+	platform: Platform
+	/**
+	 * Runtime version of the Vitest server.
+	 * The same as calling `process.version` on the server.
+	 */
+	version: string
+	/**
+	 * Name of the browser provider.
+	 */
+	provider: string
+	/**
+	 * Name of the current browser.
+	 */
+	browser: string
+	/**
+	 * Available commands for the browser.
+	 * @see {@link https://vitest.dev/guide/browser/commands}
+	 */
+	commands: BrowserCommands
+	/**
+	 * Serialized test config.
+	 */
+	config: SerializedConfig
 }>({} as any, {
 	get(target, prop) {
 		return (target as any)[prop] ?? (ctx?.server as any)[prop]
