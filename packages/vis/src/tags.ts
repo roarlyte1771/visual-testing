e636b46f2713f3c3461619a3826b40df386fd7c0
Homa Wong <homawong@gmail.com>
2024-10-28 10:37:20 -0700

    fix: remove dep on StoryContext

--- packages/vis/src/tags.ts
+++ packages/vis/src/tags.ts
@@ -1,6 +1,11 @@
-import type { StoryContext } from '@storybook/react'
-
-export function shouldTakeSnapshot(ctx: { story?: StoryContext }) {
+/**
+ * Determine should snapshot be taken.
+ *
+ * not story: false
+ * no `snapshot` tag: false
+ * disabled by `!snapshot` tag: false
+ */
+export function shouldTakeSnapshot(ctx: { story?: { tags: string[] } }) {
 	if (!ctx.story?.tags) return false
 	return ctx.story.tags.lastIndexOf('!snapshot') < ctx.story.tags.lastIndexOf('snapshot')
 }
