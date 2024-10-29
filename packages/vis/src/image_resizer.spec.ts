78658c5b13213a385443ceca78f539538ddbc179
unional <homawong@gmail.com>
2024-10-29 00:13:05 -0700

    test: cover image resizer

--- packages/vis/src/image_resizer.spec.ts
+++ packages/vis/src/image_resizer.spec.ts
@@ -1,27 +1,32 @@
 import { toImageData } from './image_data'
-import { createImageResizer } from './image_resizer'
+import { createImageResizer, createImageResizer2 } from './image_resizer'
 import { UNI_PNG_BASE64 } from './testing/constants'
 
-it('takes less than 200ms to resize 16 times', async () => {
+it('returns the same image if no resize is needed', async () => {
 	const image = await toImageData(UNI_PNG_BASE64)
-	const startTime = new Date().getTime()
-	await createImageResizer({ width: image.width * 4, height: image.height * 4 })(image)
-	const endTime = new Date().getTime()
-	const executionTime = endTime - startTime
-	expect(executionTime).toBeLessThan(200)
+	let result = await createImageResizer(image)(image)
+	expect(result).toBe(image)
+	result = await createImageResizer2(image)(image)
+	expect(result).toBe(image)
 })
 
-it.skip('takes less than 200ms to resize 16 times (average)', async () => {
-	const image = await toImageData(UNI_PNG_BASE64)
-	const length = 10
-	const startTime = new Date().getTime()
+it('takes less than 200ms to resize 16 times', async () => {
+	await testResize(createImageResizer, await toImageData(UNI_PNG_BASE64), 4, 1)
+})
 
+it('takes less than 200ms to resize 16 times (createImageResizer2)', async () => {
+	await testResize(createImageResizer2, await toImageData(UNI_PNG_BASE64), 4, 1)
+})
+
+async function testResize(fn: typeof createImageResizer, image: ImageData, factor: number, length: number) {
+	const startTime = new Date().getTime()
+	const resize = fn({ width: image.width * factor, height: image.height * factor })
 	await Promise.all(
 		Array.from({ length }).map(async () => {
-			await createImageResizer({ width: image.width * 4, height: image.height * 4 })(image)
+			await resize(image)
 		}),
 	)
 	const endTime = new Date().getTime()
 	const executionTime = endTime - startTime
 	expect(executionTime / length).toBeLessThan(200)
-})
+}
