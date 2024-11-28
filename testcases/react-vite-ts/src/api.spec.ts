f78b2b9a4022dfe94fb8528a651411830c0e455a
unional <homawong@gmail.com>
2024-11-28 09:53:05 -0800

    chore: enable dev script development

---
+++ testcases/react-vite-ts/src/api.spec.ts
@@ -0,0 +1,6 @@
+import { commands } from 'storybook-addon-vis'
+import { expect, it } from 'vitest'
+
+it('should not expose internal commands', () => {
+	expect(commands).to.not.have.property('setupSuite')
+})
