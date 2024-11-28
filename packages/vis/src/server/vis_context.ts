d85e95addfa9f3c1d25c30dd916faa919657186b
unional <homawong@gmail.com>
2024-11-28 12:41:44 -0800

    chore: pop previous type as comment

--- packages/vis/src/server/vis_context.ts
+++ packages/vis/src/server/vis_context.ts
@@ -5,3 +5,27 @@ type VisContext = {
 }
 
 export const visContext: VisContext = {}
+
+// export type VisState = {
+// 	snapshotRootDir?: string | undefined
+// 	snapshotRootPath?: string | undefined
+// 	timeout?: number | undefined
+// 	projectPath: string
+// 	testTimeout?: number | undefined
+// 	hookTimeout?: number | undefined
+// 	platform: string
+// }
+
+// export type VisContext = {
+// 	options?: VisOptions
+// 	state: VisState
+// 	suite: Record<
+// 		string,
+// 		{
+// 			baselineDir: string
+// 			resultDir: string
+// 			diffDir: string
+// 			tasks: Record<string, { count: number }>
+// 		}
+// 	>
+// }
