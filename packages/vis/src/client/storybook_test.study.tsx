f8f8c8da4f80f75913c0dde89dbfbe11d23b4dd3
Homa Wong <homawong@gmail.com>
2024-12-02 19:22:57 -0800

    test: page

--- packages/vis/src/client/storybook_test.study.tsx
+++ packages/vis/src/client/storybook_test.study.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from '@storybook/react'
 import { screen } from '@storybook/test'
+import { page } from '@vitest/browser/context'
 import { expect, test } from 'vitest'
 import * as stories from './image_data.stories.js'
 
@@ -9,4 +10,10 @@ test('using screen to access elements in story', async () => {
 	await ConversionRoundtrip.run()
 	const img = screen.getByTestId('img')
 	expect(img).toBeInTheDocument()
+})
+
+test('using page to access elements in story', async () => {
+	await ConversionRoundtrip.run()
+	const img = page.getByTestId('img')
+	expect(img.element()).toBeInTheDocument()
 })
