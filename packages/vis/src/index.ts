5880f81fd3cdb7c2d5dbb1e4638b35454edaef82
Homa Wong <homawong@gmail.com>
2024-12-10 12:44:10 -0800

    refactor: move storybook to top

--- packages/vis/src/index.ts
+++ packages/vis/src/index.ts
@@ -2,7 +2,7 @@ import './augment.ts'
 
 export * from './client/@vitest/browser/context.ts'
 export * from './client/expect.to_match_image_snapshot.ts'
-export * from './client/storybook/preview.ts'
+export * from './storybook/preview.ts'
 export * from './client/param.ts'
 export * from './shared/trim_common_folder.ts'
 export * from './shared/types.ts'
