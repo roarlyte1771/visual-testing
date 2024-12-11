0d1b7e5b302bd252f0c4baa0d2c228934cb19fbe
Homa Wong <homawong@gmail.com>
2024-12-10 21:56:39 -0800

    chore: fix storybook

--- packages/vis/.storybook/main.ts
+++ packages/vis/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
 		getAbsolutePath('@storybook/addon-essentials'),
 		getAbsolutePath('@storybook/addon-interactions'),
 		getAbsolutePath('@storybook/experimental-addon-test'),
-		'./local-preset.ts',
+		'./local-preset.js',
 	],
 	framework: {
 		name: getAbsolutePath('@storybook/react-vite'),
