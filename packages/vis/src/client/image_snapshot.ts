7f8e17bfac0d30295bb5a300322ce2639ddad012
unional <homawong@gmail.com>
2024-12-11 21:05:47 -0800

    fix: add check for running in test

--- packages/vis/src/client/image_snapshot.ts
+++ packages/vis/src/client/image_snapshot.ts
@@ -3,6 +3,10 @@ import type { ImageSnapshot, ImageSnapshotOptions } from '../shared/types.ts'
 import { commands } from './@vitest/browser/context.ts'
 
 export function imageSnapshot(options?: ImageSnapshotOptions | undefined): Promise<ImageSnapshot> {
-	const name = getCurrentTest()!.name
-	return commands.imageSnapshot(name, options)
+	const test = getCurrentTest()
+	if (!test) {
+		throw new Error('`imageSnapshot()` must be called during a test')
+	}
+
+	return commands.imageSnapshot(test.name, options)
 }
