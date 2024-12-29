17b06b2c3976d0e1ca7c4c22845df3a68ca58bc9
unional <homawong@gmail.com>
2024-12-28 23:50:47 -0800

    chore: remove stories coverage exclude

--- packages/vitest-plugin-vis/vitest.config.ts
+++ packages/vitest-plugin-vis/vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
 			exclude: [
 				'**/*.{spec,test,unit,accept,integrate,system,study,perf,stress}.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
 				'**/*.{spec,test,unit,accept,integrate,system,study,perf,stress}.*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
-				'**/*.stories.{js,mjs,jsx,tsx}',
 			],
 		},
 	},
