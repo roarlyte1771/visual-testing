edf042cbe717cc8ca5d158b962a90a72b45a39f1
unional <homawong@gmail.com>
2024-12-29 23:18:42 -0800

    refactor: rename folder

--- packages/storybook-addon-vis/src/storybook/preview.ts
+++ packages/storybook-addon-vis/src/storybook/preview.ts
@@ -1,8 +1,8 @@
 import { expect } from '@storybook/test'
 import type { ProjectAnnotations, Renderer, StoryContext } from 'storybook/internal/types'
 import { toMatchImageSnapshot } from '../client/expect.to_match_image_snapshot.ts'
+import { toMatchImageSnapshot2 } from '../client/expect/to_match_image_snapshot.ts'
 import { state } from '../client/state.ts'
-import { toMatchImageSnapshot2 } from '../client/to_match_image_snapshot/to_match_image_snapshot.ts'
 
 expect.extend({ toMatchImageSnapshot, toMatchImageSnapshot2 })
 
