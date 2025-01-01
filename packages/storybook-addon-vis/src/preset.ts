8030b1a131476e4908c086d5f1c17b51a846210c
unional <homawong@gmail.com>
2024-12-31 21:49:45 -0800

    fix : add viteFinal

--- packages/storybook-addon-vis/src/preset.ts
+++ packages/storybook-addon-vis/src/preset.ts
@@ -1,7 +1,13 @@
 import preview from './preview.ts'
+import { storybookVis } from './vitest-plugin.ts'
 
 export default {
 	previewAnnotations(entry = []) {
 		return [...entry, preview]
 	},
+}
+
+export function viteFinal(config: any, options: any = {}) {
+	config.plugins.push(storybookVis(options))
+	return config
 }
