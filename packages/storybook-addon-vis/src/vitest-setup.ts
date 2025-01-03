ca68f7fca68d225d495697214f397cbfc1e1cedb
unional <homawong@gmail.com>
2025-01-03 00:12:54 -0800

    fix: remove extra extend

--- packages/storybook-addon-vis/src/vitest-setup.ts
+++ packages/storybook-addon-vis/src/vitest-setup.ts
@@ -1,6 +1,5 @@
 // `storybook-addon-vis/vitest-setup` provides code needed in `vitest.setup.ts`.
 import { createVis } from 'vitest-plugin-vis/setup'
 import { commands } from './client/vitest_proxy.ts'
-import './vitest-setup/expect_extend.ts'
 
 export const vis = createVis(commands)
