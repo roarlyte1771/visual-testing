ebd7aca19842a95ad8a45dd5d92c949c4f998d1f
unional <homawong@gmail.com>
2025-01-04 15:50:36 -0800

    refactor: improve coverage

--- packages/storybook-addon-vis/src/client/storybook/param.ts
+++ packages/storybook-addon-vis/src/client/storybook/param.ts
@@ -9,3 +9,6 @@ export function defineAutoSnapshotParam(snapshot: ToMatchImageSnapshotOptions) {
 
 // This fix the inferred type cannot be named error
 export type { ToMatchImageSnapshotOptions }
+export function isSnapshotEnabled(tags: string[]) {
+	return tags.lastIndexOf('!snapshot') < tags.lastIndexOf('snapshot')
+}
--- packages/storybook-addon-vis/src/preview/vis_annotation.ts
+++ packages/storybook-addon-vis/src/preview/vis_annotation.ts
@@ -1,12 +1,10 @@
 import type { ProjectAnnotations, Renderer, StoryContext } from 'storybook/internal/types'
 import { setAutoSnapshotOptions } from 'vitest-plugin-vis'
+import { isSnapshotEnabled } from '../client/storybook/param.ts'
 import { getCurrentTest } from '../client/vitest_proxy.ts'
 
 export const visAnnotations = {
 	beforeEach(ctx: StoryContext) {
-		const tags = ctx.tags
-		const enable = !tags ? false : tags.lastIndexOf('!snapshot') < tags.lastIndexOf('snapshot')
-		const test = getCurrentTest()
-		setAutoSnapshotOptions(test, { enable, ...ctx.parameters?.snapshot })
+		setAutoSnapshotOptions(getCurrentTest(), { enable: isSnapshotEnabled(ctx.tags), ...ctx.parameters?.snapshot })
 	},
 } satisfies ProjectAnnotations<Renderer>
