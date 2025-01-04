b17a6b0c48473bb459c7e202bef4e525d5c58273
unional <homawong@gmail.com>
2025-01-04 13:20:59 -0800

    chore: exclude browse_provider from coverage

--- packages/vitest-plugin-vis/vitest.config.ts
+++ packages/vitest-plugin-vis/vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 			exclude: [
 				'**/*.{spec,test,unit,accept,integrate,system,study,perf,stress}.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
 				'**/*.{spec,test,unit,accept,integrate,system,study,perf,stress}.*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}',
+				'src/server/browser_provider',
 			],
 		},
 	},
