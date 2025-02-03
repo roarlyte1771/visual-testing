0d2f17c3b3f5be62f89bf299861743da908df815
unional <homawong@gmail.com>
2025-02-03 02:24:29 -0800

    chore: add tags to options

--- packages/storybook-addon-vis/src/preview/vis_annotation.ts
+++ packages/storybook-addon-vis/src/preview/vis_annotation.ts
@@ -1,10 +1,9 @@
 import type { ProjectAnnotations, Renderer, StoryContext } from 'storybook/internal/types'
 import { setAutoSnapshotOptions } from 'vitest-plugin-vis'
 import { isSnapshotEnabled } from '../client/storybook/param.ts'
-import { getCurrentTest } from '../client/vitest_proxy.ts'
 
 export const visAnnotations = {
 	beforeEach(ctx: StoryContext) {
-		setAutoSnapshotOptions(getCurrentTest(), { enable: isSnapshotEnabled(ctx.tags), ...ctx.parameters?.snapshot })
+		setAutoSnapshotOptions({ enable: isSnapshotEnabled(ctx.tags), ...ctx.parameters?.snapshot, tags: ctx.tags })
 	},
 } satisfies ProjectAnnotations<Renderer>
