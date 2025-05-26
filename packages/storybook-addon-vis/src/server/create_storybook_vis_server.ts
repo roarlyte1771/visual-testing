786e8b7e58d1d5e191b6e9d0aaaa67e5f4e1f6fe
unional <homawong@gmail.com>
2025-05-25 17:21:14 -0700

    chore: found snapshot
    copying code from `vitest-plugin-vis`.
    Else running into:
    
    ```
    ReferenceError: exports is not defined
            at file:///home/runner/work/visual-testing/visual-testing/node_modules/.pnpm/type-plus@8.0.0-beta.7/node_modules/type-plus/cjs/index.js:16:23
    ```
    
    caused by:
    https://stackoverflow.com/questions/79335257/referenceerror-exports-is-not-defined-after-update-to-node-22-12-from-node-22

--- packages/storybook-addon-vis/src/server/create_storybook_vis_server.ts
+++ packages/storybook-addon-vis/src/server/create_storybook_vis_server.ts
@@ -1,16 +1,66 @@
+import { globSync } from 'glob'
+import ci from 'is-ci'
+import memoize from 'memoize'
+import { platform } from 'node:process'
+import { resolve } from 'pathe'
 import type { StorybookVisOptions } from './vis_options.ts'
 
+const SNAPSHOT_ROOT_DIR = '__vis__'
+const BASELINE_DIR = '__baselines__'
+const RESULT_DIR = '__results__'
+const DIFF_DIR = '__diffs__'
+
 export function createStorybookVisServer(options: StorybookVisOptions) {
+	const visSuites = options.visSuites ?? [{ snapshotRootDir: SNAPSHOT_ROOT_DIR }]
 	return {
 		options,
-		async getImageSnapshotResults(taskId: string, taskPath: string) {
-			// TODO: Implement actual image snapshot comparison logic
-			return {
-				key: `${taskId}-${taskPath}`,
-				baseline: undefined,
-				diff: undefined,
-				result: undefined,
-			}
+		async getImageSnapshotResults(name: string, importPath: string) {
+			return visSuites
+				.map((suite) => {
+					const snapshotRootDir =
+						(typeof suite.snapshotRootDir === 'function'
+							? memoize(suite.snapshotRootDir)({
+									ci,
+									platform,
+								})
+							: suite.snapshotRootDir) ?? SNAPSHOT_ROOT_DIR
+					const snapshotSubpathFn =
+						typeof suite.snapshotSubpath === 'function'
+							? memoize(suite.snapshotSubpath)
+							: memoize(({ subpath }: { subpath: string }) =>
+									trimCommonFolder(subpath.startsWith('./') ? subpath.slice(2) : subpath),
+								)
+					const snapshotSubpath = snapshotSubpathFn({ subpath: importPath })
+					return {
+						snapshotRootDir,
+						snapshotSubpath,
+					}
+				})
+				.flatMap(({ snapshotRootDir, snapshotSubpath }) => {
+					const taskGlob = `${name.replace(/[^a-z0-9]/gi, '-').toLowerCase()}-*.png`
+					const baselineGlob = resolve(snapshotRootDir, BASELINE_DIR, snapshotSubpath, taskGlob)
+					const resultGlob = resolve(snapshotRootDir, RESULT_DIR, snapshotSubpath, taskGlob)
+					const diffGlob = resolve(snapshotRootDir, DIFF_DIR, snapshotSubpath, taskGlob)
+
+					const baselineFiles = globSync(baselineGlob)
+					const resultFiles = globSync(resultGlob)
+					const diffFiles = globSync(diffGlob)
+					return {
+						baselineGlob,
+						resultGlob,
+						diffGlob,
+						baselineFiles,
+						resultFiles,
+						diffFiles,
+					}
+				})
 		},
 	}
+}
+
+function trimCommonFolder(suiteName: string) {
+	const suiteDir = suiteName.split('/', 1)[0]!
+	if (['tests', 'test', 'src', 'source', 'js', 'ts', 'lib'].includes(suiteDir))
+		return suiteName.slice(suiteDir.length + 1)
+	return suiteName
 }
