2a17a0c4bc1f2a711b2536ce4309cc6940aba69a
Lee Beydoun <leebeydoun@Lees-MacBook-Air.local>
2025-05-26 21:54:28 +0100

    Fixed to sb v9 compat

--- packages/storybook-addon-vis/scripts/prepublish-checks.js
+++ packages/storybook-addon-vis/scripts/prepublish-checks.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env zx
 
-import { readFile } from 'node:fs/promises'
 import boxen from 'boxen'
 import dedent from 'dedent'
-import { globalPackages as globalManagerPackages } from 'storybook/internal/manager/globals'
-import { globalPackages as globalPreviewPackages } from 'storybook/internal/preview/globals'
+import { readFile } from 'node:fs/promises'
+import { globalPackages as globalManagerPackages } from 'storybook/manager/globals'
+import { globalPackages as globalPreviewPackages } from 'storybook/preview/globals'
 
 const packageJson = await readFile('./package.json', 'utf8').then(JSON.parse)
 
