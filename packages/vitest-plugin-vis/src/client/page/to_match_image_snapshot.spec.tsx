60065a892ba8a95850f226dd3552f8081bf8f895
unional <homawong@gmail.com>
2025-05-25 17:21:13 -0700

    test: throw on concurrent

--- packages/vitest-plugin-vis/src/client/page/to_match_image_snapshot.spec.tsx
+++ packages/vitest-plugin-vis/src/client/page/to_match_image_snapshot.spec.tsx
@@ -8,3 +8,11 @@ it('throws when not running in a test', async ({ expect }) => {
 	ctx.getCurrentTest = () => undefined as any
 	await expect(() => page.toMatchImageSnapshot()).rejects.toThrow('`toMatchImageSnapshot()` must be called in a test.')
 })
+
+it('throws an error when running in a concurrent test', ({ expect }) => {
+	ctx.getCurrentTest = () => ({ concurrent: true }) as any
+	expect(() => page.toMatchImageSnapshot()).rejects.toThrow(
+		'`toMatchImageSnapshot()` cannot be called in a concurrent test because ' +
+			"concurrent tests run at the same time in the same iframe and affect each other's environment.",
+	)
+})
--- packages/vitest-plugin-vis/src/client/page/to_match_image_snapshot.ts
+++ packages/vitest-plugin-vis/src/client/page/to_match_image_snapshot.ts
@@ -10,4 +10,11 @@ export async function toMatchImageSnapshot(this: BrowserPage) {
 	if (!test) {
 		throw new Error('`toMatchImageSnapshot()` must be called in a test.')
 	}
+
+	if (test.concurrent) {
+		throw new Error(
+			'`toMatchImageSnapshot()` cannot be called in a concurrent test because ' +
+				"concurrent tests run at the same time in the same iframe and affect each other's environment.",
+		)
+	}
 }
