78f850955b0e24ec728c5f57fcb3d36f21562323
unional <homawong@gmail.com>
2025-05-25 17:21:14 -0700

    chore: add storysource

--- packages/storybook-addon-vis/.storybook/main.ts
+++ packages/storybook-addon-vis/.storybook/main.ts
@@ -1,10 +1,11 @@
-import { dirname, join } from 'node:path'
 import type { StorybookConfig } from '@storybook/react-vite'
+import { dirname, join } from 'node:path'
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
 		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-essentials'),
+		'@storybook/addon-storysource',
 		getAbsolutePath('@storybook/experimental-addon-test'),
 		'./local-preset.js',
 	],
