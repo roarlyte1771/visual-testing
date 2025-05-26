fbaa75be2421bf70e0a7a7a296f307d8564421e7
unional <homawong@gmail.com>
2025-05-25 17:21:13 -0700

    fix: Use `hasImageSnapshotAction` from `vitest-plugin-vis`

---
+++ .changeset/eight-apples-reply.md
@@ -0,0 +1,5 @@
+---
+"storybook-addon-vis": patch
+---
+
+Use `hasImageSnapshotAction` from `vitest-plugin-vis`.
--- packages/storybook-addon-vis/src/client/has_image_snapshot.ts
+++ packages/storybook-addon-vis/src/client/has_image_snapshot.ts
@@ -1,4 +1,4 @@
-import { type ImageSnapshotIdOptions, toTaskId } from 'vitest-plugin-vis/client-api'
+import { hasImageSnapshotAction, type ImageSnapshotIdOptions } from 'vitest-plugin-vis/client-api'
 import { ctx } from './ctx.ts'
 import { commands } from './vitest_proxy.ts'
 
@@ -9,19 +9,5 @@ export function hasImageSnapshot(options?: ImageSnapshotIdOptions | undefined) {
 	const test = ctx.getCurrentTest()
 	if (!test) return false
 
-	const taskId = toTaskId(test)
-	const isAutoSnapshot = !!test.meta.vis?.isAutoSnapshot
-	if (options?.customizeSnapshotId) {
-		return commands
-			.imageSnapshotNextIndex(taskId)
-			.then((index) =>
-				commands.hasImageSnapshot(
-					taskId,
-					options.customizeSnapshotId!({ id: taskId, index, isAutoSnapshot }),
-					isAutoSnapshot,
-				),
-			)
-	}
-
-	return commands.hasImageSnapshot(taskId, undefined, isAutoSnapshot)
+	return hasImageSnapshotAction(commands, test, options)
 }
