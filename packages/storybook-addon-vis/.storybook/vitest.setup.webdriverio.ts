a2325cbf962b6a7bb1866e31ef15a36ea1643f29
Homa Wong <homawong@gmail.com>
2025-05-25 17:21:14 -0700

    chore: fix wd setup

--- packages/storybook-addon-vis/.storybook/vitest.setup.webdriverio.ts
+++ packages/storybook-addon-vis/.storybook/vitest.setup.webdriverio.ts
@@ -1,12 +1,11 @@
 import { setProjectAnnotations } from '@storybook/react'
 import { beforeAll } from 'vitest'
-import * as visPreview from '../src/preview.ts'
-import { vis } from '../src/vitest-setup.ts'
+import { vis, visAnnotations } from '../src/vitest-setup.ts'
 import * as projectAnnotations from './preview.ts'
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const project = setProjectAnnotations([projectAnnotations, visPreview])
+const project = setProjectAnnotations([projectAnnotations, visAnnotations])
 
 beforeAll(project.beforeAll)
 
